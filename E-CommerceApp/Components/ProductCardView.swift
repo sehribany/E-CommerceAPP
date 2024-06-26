5d957c8d63d6ce593045187259289e77f287d08f
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-06-26 13:56:19 +0300

    ProductCardView

---
+++ E-CommerceApp/Components/ProductCardView.swift
@@ -0,0 +1,56 @@
+//
+//  ProductCardView.swift
+//  E-CommerceApp
+//
+//  Created by Şehriban Yıldırım on 25.06.2024.
+//
+
+import SwiftUI
+
+struct ProductCardView: View {
+    @EnvironmentObject var cartManager: CartManager
+    var product: Product
+    
+    var body: some View {
+        ZStack{
+            Color("kSecondary")
+            ZStack(alignment: .bottomTrailing){
+                VStack(alignment: .leading){
+                    Image(product.image)
+                        .resizable()
+                        .frame(width: 175, height: 160)
+                        .cornerRadius(12)
+                    
+                    Text(product.name)
+                        .font(.headline)
+                        .padding(.vertical, 1)
+                    
+                    Text(product.supplies)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                        .padding(.vertical, 0.5)
+                    
+                    Text("$ \(product.price)")
+                        .bold()
+                }
+                
+                Button{
+                    cartManager.addToCart(product: product)
+                } label:{
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .foregroundColor(Color("kPrimary"))
+                        .frame(width: 35, height: 35)
+                        .padding(.trailing)
+                }
+            }
+        }
+        .frame(width: 185, height: 260)
+        .cornerRadius(15)
+    }
+}
+
+#Preview {
+    ProductCardView(product: productList[0])
+        .environmentObject(CartManager())
+}
